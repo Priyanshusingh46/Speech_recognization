3ccf44970696c8ace3cc7fbffbc69a9a3d409f4b
Priyanshu46 <priyanshusingh1877@gmail.com>
2023-04-12 22:24:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import css from "./style.css";
+import bg from "./Images/bg.jpg"
 function App() {
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppOuterContainer">
+      <h2 id="headingpara">Welcome to SpeechRecognition App ~ Priyanshu Singh</h2>
+      <div className="textwritten">
+      <p id="headingpara">{transcript}</p>
+      </div>
+     
+      <div className="buttondiv">
+      <button id="buttonhai"onClick={SpeechRecognition.startListening}>Start</button>
+      <button id="buttonhai" onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button id="buttonhai" onClick={resetTranscript}>Reset</button>
+      </div>
     </div>
   );
 }
